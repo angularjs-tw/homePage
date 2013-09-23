07d872c46b168e6231a93012cc0173ba8d75b51d
Justin <yuting1987@gmail.com>
2013-09-23 19:50:23 +0800

    Update

---
+++ views/scripts/223f7039.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("homePageApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("homePageApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
